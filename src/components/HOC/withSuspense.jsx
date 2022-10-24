976abc33c5bd19a129d352832e7e8047461882a2
abulai <rakhatulyabylai@gmail.com>
2022-10-24 18:43:06 +0600

    Updates

--- src/components/HOC/withSuspense.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export let withSuspense = (Component) =>{
-    return (props)=>{
-        return <React.Suspense fallback={"Loading"}>
-                 <Component {...props}/> 
-            </React.Suspense>
-    }
-}
